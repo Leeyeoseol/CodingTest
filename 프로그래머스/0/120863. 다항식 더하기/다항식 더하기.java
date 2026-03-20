0fbc2530dbf32abb644b9b5132b060c2b7976f3b
이여설 <iyeoseol096@gmail.com>
2026-03-20 22:27:50 +0900

    [level 0] Title: 다항식 더하기, Time: 13.45 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -0,0 +1,55 @@
+class Solution {
+    public String solution(String polynomial) {
+        String answer = "";
+        int xSum = 0;
+        int numSum = 0;
+        
+        String[] poly = polynomial.split(" \\+ ") ;
+        for(int i=0;i<poly.length;i++){
+            if(poly[i].contains("x")){//x포함이라면
+                String num=poly[i].replace("x","");
+                if(num.equals("")){
+                    xSum+=1;
+                }else{
+                    xSum+=Integer.parseInt(num);
+                }
+            }else{
+                numSum+=Integer.parseInt(poly[i]);
+            }
+        }
+        if(xSum > 0){
+            if(xSum == 1){
+                answer += "x";
+            } else {
+                answer += xSum + "x";
+            }
+        }
+        if(numSum > 0){
+            if(xSum > 0){
+                answer += " + ";
+            }
+            answer += numSum;
+        }
+        if(answer.equals("")){
+            answer = "0";
+        }
+        return answer;
+    }
+}
+
+/*
+if(poly[i].contains("x")) {
+    // 1. x 제거
+    String num = poly[i].replace("x", "");
+
+    // 2. 계수 처리
+    if(num.equals("")) {
+        xSum += 1;
+    } else {
+        xSum += Integer.parseInt(num);
+    }
+} else {
+    // 숫자니까 그냥 더하기
+    numSum += Integer.parseInt(poly[i]);
+}
+*/
