1672269b6cd17f5f27619e62bd446401e70bf1b4
이여설 <iyeoseol096@gmail.com>
2026-04-03 17:30:57 +0900

    [level 2] Title: H-Index, Time: 20.09 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        int max = citations[citations.length - 1];//마지막꺼
+        for(int i=max;i>=0;i--){
+            for(int j=0;j<citations.length;j++){
+                if (citations[j]>=i&&citations.length-j>=i) return i;
+            }
+        }
+        return 0;
+    }
+}
