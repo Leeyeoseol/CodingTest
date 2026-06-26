71bd60f084618e6a623c0c093de1e20a64525fd4
이여설 <iyeoseol096@gmail.com>
2026-06-26 20:07:09 +0900

    [level 0] Title: 배열 조각하기, Time: 0.06 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+
+        int start =0;
+        int end=arr.length-1;
+        
+        for(int i=0;i<query.length;i++){
+            if(i%2==0){
+                end=start+query[i];
+            }else{
+                start =start+query[i];
+            }
+        }
+        int []answer=new int[end-start+1];
+        for(int i=0;i<answer.length;i++){
+            answer[i]=arr[start+i];
+        }
+        return answer;
+    }
+}
