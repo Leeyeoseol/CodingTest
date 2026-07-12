1fb67f0c12026e738b4010ca3f5a94e864083caf
이여설 <iyeoseol096@gmail.com>
2026-07-13 07:52:33 +0900

    [level 0] Title: 그림 확대, Time: 6.43 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length*k];
+        int num=0;
+        for(int i=0;i<picture.length;i++){
+             String str = ""; 
+            for(int j=0;j<picture[i].length();j++){
+                for (int l=0;l<k;l++){
+                    str+=picture[i].charAt(j);
+                }
+            }
+            for(int a=0;a<k;a++){
+                answer[num++]=str;
+            }
+            
+        }
+        return answer;
+    }
+}
+//charAt()
+
+//for문으로, 가로먼저 늘리고 세로 늘리기
