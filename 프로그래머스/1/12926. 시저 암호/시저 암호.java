bcb580acf7f6d371fea36dcb61f0e528b7a2d421
LEE YEO SEOL <iyeoseol096@gmail.com>
2026-08-10 13:21:56 +0900

    [level 1] Title: 시저 암호, Time: 1.10 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb=new StringBuilder();
+        
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            
+            if(Character.isUpperCase(ch)){
+                ch=(char)((ch- 'A'+n)%26 +'A');
+            }else if(Character.isLowerCase(ch)){
+                ch=(char)((ch - 'a'+ n)%26 +'a');
+            }
+            sb.append(ch);//추가
+        }
+        
+        return sb.toString();
+    }
+}
