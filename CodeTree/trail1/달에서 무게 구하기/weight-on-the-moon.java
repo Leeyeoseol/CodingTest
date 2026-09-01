10f503a2b02389ac07826b1ffe78ab5e841f3f1e
LEE YEO SEOL <iyeoseol096@gmail.com>
2026-09-01 09:32:25 +0900

    [Passed] 달에서 무게 구하기 (107ms, 9MB)

---
+++ CodeTree/trail1/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int w=13;
+        double m=0.165;
+
+        System.out.printf("%d * %f = %.6f", w, m, (w * m));
+
+    }
+}
