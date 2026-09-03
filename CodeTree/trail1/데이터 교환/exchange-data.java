9dc825b60d321976b61645624d3c8172391db37c
LEE YEO SEOL <iyeoseol096@gmail.com>
2026-09-03 09:26:22 +0900

    [Passed] 데이터 교환 (91ms, 9MB)

---
+++ CodeTree/trail1/데이터 교환/exchange-data.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a=5;
+        int b=6;
+        int c=7;
+        int tempa=a;
+        int tempb=b;
+        int tempc=c;
+
+        a=tempc;
+        b=tempa;
+        c=tempb;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
