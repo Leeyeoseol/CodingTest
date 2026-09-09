392f6c55be4e470eef712a52dc44080caab8fd93
LEE YEO SEOL <iyeoseol096@gmail.com>
2026-09-09 13:33:37 +0900

    [Passed] 합을 복사하기 (108ms, 9MB)

---
+++ CodeTree/trail1/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a=1;
+        int b=2;
+        int c=3;
+        int temp=a+b+c;
+        a=temp;
+        b=temp;
+        c=temp;
+        System.out.print(a+" "+b+" "+c);
+    }
+}
